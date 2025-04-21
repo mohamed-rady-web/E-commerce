09dda7bc0ffaeaa866905816e34762af56e015ab
Mohamed Rady <mohamed.rady99990@gmail.com>
2025-04-21 20:31:35 +0200

    editing some end points

--- Routes/ContactUsRoute.js
+++ Routes/ContactUsRoute.js
@@ -1,7 +1,6 @@
 const express =require('express');
 const router = express.Router();
 const ContactController = require('../Controllers/ContactUsController');
-const Middlewares= require('../Middlewares/auth');
 
  router.get('/ContactUs', ContactController.GetContact);
  router.post('/Report' ,ContactController.Report);
